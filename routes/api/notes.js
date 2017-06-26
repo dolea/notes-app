f2f6fbedcc6badf256566391d5b6c5ddaba619b8
Daniel Olea <daniel.olea@lastminute.com>
2017-06-26 18:57:35 +0200

    Duplicate coupled code for now

---
+++ routes/api/notes.js
@@ -0,0 +1,22 @@
+var router = require('express').Router();
+
+// api/notes
+router.post('/', function(req, res) {
+  Note.create({
+            message : req.body.message,
+        }, 
+        function (err, note) {
+            if (err) return res.status(500).send("There was a problem adding the note to the database.");
+            res.status(200).send(note);
+        });
+});
+
+// api/notes
+router.get('/', function(req, res) {
+  Note.find({}, function (err, notes) {
+        if (err) return res.status(500).send("There was a problem finding the notes.");
+        res.status(200).send(notes);
+    });
+});
+
+module.exports = router;
