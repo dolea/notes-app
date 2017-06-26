14f63545052e8e58a95c1a8b43ace501ea5278f5
Daniel Olea <daniel.olea@lastminute.com>
2017-06-26 18:56:12 +0200

    Improve endpoint management

---
+++ routes/notes.js
@@ -0,0 +1,13 @@
+var router = require('express').Router();
+
+// api/notes
+router.post('/', function(req, res) {
+  //TODO:
+});
+
+// api/notes
+router.get('/', function(req, res) {
+  //TODO:
+});
+
+module.exports = router;
