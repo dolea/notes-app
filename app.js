2f9761d2ee39019e60054cd7fc7375579c4b69bc
Daniel Olea <daniel.olea@lastminute.com>
2017-06-26 18:39:19 +0200

    Quick & dirty post & get ops over notes

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
+var UserController = require('./note/NoteController');
+app.use('/notes', UserController);
+
 module.exports = app;
---
+++ note/note-controller.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+var Note = require('./Note');
+
+// TODO: remove coupling
+router.post('/', function (req, res) {
+
+    Note.create({
+            message : req.body.message,
+        }, 
+        function (err, note) {
+            if (err) return res.status(500).send("There was a problem adding the note to the database.");
+            res.status(200).send(note);
+        });
+
+});
+
+router.get('/', function (req, res) {
+
+    Note.find({}, function (err, notes) {
+        if (err) return res.status(500).send("There was a problem finding the notes.");
+        res.status(200).send(notes);
+    });
+
+});
+
+module.exports = router;
