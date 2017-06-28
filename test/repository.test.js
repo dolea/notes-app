c8b9c4f78cae2f6d124115d9d9d0c52ca556cf3a
Daniel Olea <daniel@explodedview.es>
2017-06-28 23:22:08 +0200

    For the time being join all repository integration tests as they actually work in collaboration for some operations

--- test/repository.test.js
+++ test/repository.test.js
@@ -1,15 +1,48 @@
 'use strict';
 
 const mongoose = require('mongoose');
-// follow mongoose recommendation of using own promise library
 mongoose.Promise = require('bluebird');
 const db = mongoose.connect('mongodb://localhost/test');
 
 const expect = require("chai").expect;
 
+const BookmarksRepository = require('../libs/repository/bookmarks-repository');
+const DbBookmark = require('../libs/repository/mongo-bookmark');
 const NoteRepository = require('../libs/repository/note-repository');
 const DbNote = require('../libs/repository/mongo-note');
-const DbBookmark = require('../libs/repository/mongo-bookmark');
+
+describe("BookmarksRepository",() => {
+
+    describe("User has no bookmarks",() => {
+        after(cleanUp);
+        it("should store a bookmark", async () => {
+            const anyNoteId = mongoose.Types.ObjectId();
+            await new BookmarksRepository().addBookmarkedNote("::any user::", anyNoteId);
+
+            return DbBookmark.findOne().then(bookmark => {
+                expect(bookmark._id).to.equal("::any user::");
+                expect(bookmark.bookmarkedNotes).to.deep.equal([anyNoteId])
+            });
+        });
+    });
+
+    describe("User bookmarks two notes",() => {
+        after(cleanUp);
+        it("should add bookmarks to user", async () => {
+            const anyNoteId = mongoose.Types.ObjectId();
+            await new BookmarksRepository().addBookmarkedNote("::any user::", anyNoteId);
+
+            const anotherNoteId = mongoose.Types.ObjectId();
+            await new BookmarksRepository().addBookmarkedNote("::any user::", anotherNoteId);
+
+
+            return DbBookmark.findOne().then(bookmark => {
+                expect(bookmark._id).to.equal("::any user::");
+                expect(bookmark.bookmarkedNotes).to.deep.equal([anyNoteId, anotherNoteId])
+            });
+        })
+    });
+});
 
 describe("NoteRepository",() => {
     after(cleanUp);
