6480f64268cfac2560aef05ebe9a516d220881d8
Daniel Olea <daniel@explodedview.es>
2017-06-29 00:39:48 +0200

    Add additional test cases. These should be mocked instead of faked.

--- usecases/notes/display-notes.test.js
+++ usecases/notes/display-notes.test.js
@@ -1,21 +1,37 @@
 'use strict';
 
 const DisplayNotesUseCase = require('./display-notes');
-const NotesRepository = require('../../repository/mongo/note-repository');
-const sinon = require('sinon');
 const expect = require("chai").expect;
 
 
 describe('display notes use case', () => {
-    it('should do anything', () => {
+    it('should call the repository to create a note', () => {
         new DisplayNotesUseCase(new FakeRepository()).onNewNote('::any message::', '::any username::', null);
-    })
+    });
+
+    it('should call the repository to retrieve a note', () => {
+        new DisplayNotesUseCase(new FakeRepository()).byId('::any note id::', null);
+    });
+
+    it('should call the repository to retrieve bookmakrs', () => {
+        new DisplayNotesUseCase(new FakeRepository()).bookmarkedBy('::any user::', null);
+    });
 });
 
 class FakeRepository {
     createNewNote(message, user) {
         expect(message).to.equal('::any message::');
         expect(user).to.equal('::any username::');
+        return Promise.resolve({});
+    }
+
+    findNoteById(noteId) {
+        expect(noteId).to.equal('::any note id::');
+        return Promise.resolve({});
+    }
+
+    findBookmarkedNotesByCreatorId(username) {
+        expect(username).to.equal('::any user::');
         return Promise.resolve({});
     }
 }
