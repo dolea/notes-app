55151fbcf99d55ed188beb5933ff3aeb582a94f1
Daniel Olea <daniel@explodedview.es>
2017-06-28 22:32:32 +0200

    Add test case for multiple bookmarks

--- test/bookmarks-repository.integration.test.js
+++ test/bookmarks-repository.integration.test.js
@@ -10,20 +10,38 @@ const expect = require("chai").expect;
 const BookmarksRepository = require('../libs/repository/bookmarks-repository');
 const DbBookmark = require('../libs/repository/mongo-bookmark');
 
-describe("Stores a bookmark",() => {
+describe("BookmarksRepository",() => {
     function cleanUp () {
         db.connection.db.dropDatabase();
     }
 
-    after(cleanUp);
+    describe("User has no bookmarks",() => {
+        after(cleanUp);
 
-    it("should store a bookmark", async () => {
-        const anyNoteId = mongoose.Types.ObjectId();
-        await new BookmarksRepository().addBookmarkedNote("::any user::", anyNoteId);
+        it("should store a bookmark", async () => {
+            const anyNoteId = mongoose.Types.ObjectId();
+            await new BookmarksRepository().addBookmarkedNote("::any user::", anyNoteId);
 
-        return DbBookmark.findOne().then(bookmark => {
-            expect(bookmark._id).to.equal("::any user::");
-            expect(bookmark.bookmarkedNotes).to.deep.equal([anyNoteId])
+            return DbBookmark.findOne().then(bookmark => {
+                expect(bookmark._id).to.equal("::any user::");
+                expect(bookmark.bookmarkedNotes).to.deep.equal([anyNoteId])
+            });
         });
+    });
+
+    describe("User bookmarks two notes",() => {
+        it("should add bookmarks to user", async () => {
+            const anyNoteId = mongoose.Types.ObjectId();
+            await new BookmarksRepository().addBookmarkedNote("::any user::", anyNoteId);
+
+            const anotherNoteId = mongoose.Types.ObjectId();
+            await new BookmarksRepository().addBookmarkedNote("::any user::", anotherNoteId);
+
+
+            return DbBookmark.findOne().then(bookmark => {
+                expect(bookmark._id).to.equal("::any user::");
+                expect(bookmark.bookmarkedNotes).to.deep.equal([anyNoteId, anotherNoteId])
+            });
+        })
     });
 });
