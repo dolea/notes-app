c24124fef6c958c6aa72251409c7ab5462473ddc
Daniel Olea <daniel@explodedview.es>
2017-06-28 19:57:32 +0200

    Minor improvements

--- libs/repository/mongo-note.js
+++ libs/repository/mongo-note.js
@@ -1,7 +1,10 @@
-var mongoose = require('mongoose');  
-var NoteSchema = new mongoose.Schema({  
-  message: String,
-    creator: String
+'use strict';
+
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema({
+    message: String,
+    author: String
 });
 mongoose.model('Note', NoteSchema);
 
