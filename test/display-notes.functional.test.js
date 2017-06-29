538f53307757dd3a09c626a46cfae0644c37efd1
Daniel Olea <daniel@explodedview.es>
2017-06-29 23:51:50 +0200

    Improve test cases description

--- test/display-notes.functional.test.js
+++ test/display-notes.functional.test.js
@@ -4,36 +4,41 @@ const request = require('request-promise');
 const app = require('../web');
 const expect = require("chai").expect;
 
-//TODO: fix conflict with repository test (trying to open a mongo connection when already there is one)
 describe('executes flow', function () {
     after(() => {app.Server.close(); app.Db.disconnect()});
 
-    it('should create a note', async () => {
-        const createdNote = await postNote({message: "test message", username: "test user"});
+    describe('POST /api/notes', function () {
+        it('should create a note', async () => {
+            const createdNote = await postNote({message: "test message", username: "test user"});
 
-        expect(createdNote.message).to.be.equal("test message");
-        expect(createdNote.author).to.be.equal("test user");
+            expect(createdNote.message).to.be.equal("test message");
+            expect(createdNote.author).to.be.equal("test user");
+        });
     });
 
-    it('should recover a created note', async () => {
-        const createdNote = await postNote({message: "test message", username: "test user"});
+    describe('GET /api/notes/:noteId', function () {
+        it('should recover a created note', async () => {
+            const createdNote = await postNote({message: "test message", username: "test user"});
 
-        const recoveredNote = await request({
-            uri: 'http://localhost:3000/api/notes/' + createdNote.noteId,
-            json: true
+            const recoveredNote = await request({
+                uri: 'http://localhost:3000/api/notes/' + createdNote.noteId,
+                json: true
+            });
+
+            expect(recoveredNote).to.be.deep.equal(createdNote);
         });
-
-        expect(recoveredNote).to.be.deep.equal(createdNote);
     });
 
-    it('should recover all notes', async () => {
-        const recoveredNotes = await request({
-            uri: 'http://localhost:3000/api/notes/',
-            json: true
-        });
+    describe('GET /api/notes', function () {
+        it('should recover all notes', async () => {
+            const recoveredNotes = await request({
+                uri: 'http://localhost:3000/api/notes/',
+                json: true
+            });
 
-        //TODO: fix weak assertion
-        expect(recoveredNotes).to.not.be.empty;
+            //TODO: fix weak assertion
+            expect(recoveredNotes).to.not.be.empty;
+        });
     });
 });
 
