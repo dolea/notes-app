0bc9dbc890ffebee4c7e850a993fccce4aaadbbc
Daniel Olea <daniel@explodedview.es>
2017-06-29 19:52:48 +0200

    Improve e2e test for note creation and retrieval

--- test/e2eFlow.test.js
+++ test/e2eFlow.test.js
@@ -1,24 +1,37 @@
 'use strict';
 
-const http = require('http');
+const request = require('request-promise');
 const app = require('../web');
 const expect = require("chai").expect;
 
 //TODO: fix conflict with repository test (trying to open a mongo connection when already there is one)
-describe('executes flow', function() {
-    it('should show contact a form', (done) => {
-        const options = {
-            host: '127.0.0.1',
-            port: 3000,
-            path: '/api/notes',
+describe('executes flow', function () {
+
+    it('should create a note', async () => {
+        const createdNote = await request({
+            uri: 'http://localhost:3000/api/notes',
             method: 'POST',
-            body: '{"message": "test message", "username": "test user"}'
-        };
+            body: {message: "test message", username: "test user"},
+            json: true
+        });
 
-        http.request(options, function(res) {
-            expect(res.statusCode).to.be.equal(200);
-            done();
-        }).end();
+        expect(createdNote.message).to.be.equal("test message");
+        expect(createdNote.author).to.be.equal("test user");
+    });
 
+    it('should recover a created note', async () => {
+        const createdNote = await request({
+            uri: 'http://localhost:3000/api/notes',
+            method: 'POST',
+            body: {message: "test message", username: "test user"},
+            json: true
+        });
+
+        const recoveredNote = await request({
+            uri: 'http://localhost:3000/api/notes/' + createdNote.noteId,
+            json: true
+        });
+
+        expect(recoveredNote).to.be.deep.equal(createdNote);
     });
 });
